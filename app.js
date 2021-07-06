0fa78df57f6a56e2139ced341ac8732bb6ab1f94
Jackie Low <jmlow74@gmail.com>
2021-07-06 16:04:16 +0100

    basic html and object set-up

---
+++ app.js
@@ -0,0 +1,58 @@
+//create morse code object
+const morseCode = {
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--.."
+}
+
+
+
+//grab html elements
+
+
+//capture typed input
+
+
+//validate input is valid
+
+
+//how to translate from typed input to morse code
+
+
+
+//run translation
+
+
+
+
+//display translation
+
+
+
+
+//clear area/clear translation
+
+
