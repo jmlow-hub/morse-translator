8fe3fe418955a4637b1f7656bda3eff1fd55e4ef
Jackie Low <jmlow74@gmail.com>
2021-07-06 22:07:30 +0100

    validation function updated to include search method

--- app.dev.js
+++ app.dev.js
@@ -2,67 +2,84 @@
 
 //create morse code object
 var morseCode = {
-  a: ".-",
-  b: "-...",
-  c: "-.-.",
-  d: "-..",
-  e: ".",
-  f: "..-.",
-  g: "--.",
-  h: "....",
-  i: "..",
-  j: ".---",
-  k: "-.-",
-  l: ".-..",
-  m: "--",
-  n: "-.",
-  o: "---",
-  p: ".--.",
-  q: "--.-",
-  r: ".-.",
-  s: "...",
-  t: "-",
-  u: "..-",
-  v: "...-",
-  w: ".--",
-  x: "-..-",
-  y: "-.--",
-  z: "--..",
-  " ": " "
+  a: " ·- ",
+  b: " -··· ",
+  c: " -·-· ",
+  d: " -·· ",
+  e: " · ",
+  f: " ··-· ",
+  g: " --· ",
+  h: " ···· ",
+  i: " ·· ",
+  j: " ·--- ",
+  k: " -·- ",
+  l: " ·-·· ",
+  m: " -- ",
+  n: " -· ",
+  o: " --- ",
+  p: " ·--· ",
+  q: "--.- ",
+  r: " ·-· ",
+  s: " ··· ",
+  t: " - ",
+  u: " ··- ",
+  v: " ···- ",
+  w: " ·-- ",
+  x: " -··- ",
+  y: " -·-- ",
+  z: " --·· ",
+  " ": " //  "
 }; //grab html elements
 
 var translateBtn = document.querySelector("button");
 var textInput = document.getElementById("textInput");
-var textOutput = document.querySelector(".morse__output__translation"); //reference array
+var textOutput = document.querySelector(".morse__output__translation"); //reference array for the morse code
 
 var letterKeys = Object.keys(morseCode); //capture typed input
 
-var words = []; //how to translate from typed input to morse code
-//run translation  on button click
+var words = []; //run translation  on button click
 
 var handleTranslate = translateBtn.addEventListener("click", function (e) {
   var input = textInput.value.toLowerCase(); //validate input is valid - if yes push letter to array, if not message to try again
 
   var validInput = function validInput() {
-    for (var i = 0; i < input.length; i++) {
-      if (letterKeys.includes(input[i])) {
-        words.push(input[i]);
-      } else {
-        textInput.value = "";
-        document.querySelector(".morse__output__title").innerHTML = "Try Again!";
+    var regex = /[^\w\s]/g;
+
+    if (input.search(regex) != -1) {
+      document.querySelector(".morse__output__title").innerHTML = "Try Again!";
+    } else {
+      for (var i = 0; i < input.length; i++) {
+        var letter = input[i];
+        words.push(letter);
       }
     }
-  };
+  }; // for(let i = 0; i < input.length; i++) {
+  //   let letter = input[i];
+  //   if((letterKeys.includes(letter))) {
+  //     words.push(letter);
+  //     console.log(words)
+  //   } else {
+  //     document.querySelector(".morse__output__title").innerHTML = "Try Again!"
+  //   }
+  // }
+  // }
+
 
   var translatePhrase = function translatePhrase() {
     var translationArr = [];
     words.forEach(function (word) {
       translationArr.push(morseCode[word]);
+      textOutput.innerHTML = translationArr.join(" ");
     });
-    textOutput.innerHTML = translationArr.join("  ");
+  };
+
+  var clear = function clear() {
+    textInput.value = null;
+    words.length = 0;
   };
 
   validInput();
   translatePhrase();
+  clear();
 }); //display translation
 //clear area/clear translation
--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 //create morse code object
 const morseCode = {
-  a: ".-",
-  b: "-...",
-  c: "-.-.",
-  d: "-..",
-  e: ".",
-  f: "..-.",
-  g: "--.",
-  h: "....",
-  i: "..",
-  j: ".---",
-  k: "-.-",
-  l: ".-..",
-  m: "--",
-  n: "-.",
-  o: "---",
-  p: ".--.",
-  q: "--.-",
-  r: ".-.",
-  s: "...",
-  t: "-",
-  u: "..-",
-  v: "...-",
-  w: ".--",
-  x: "-..-",
-  y: "-.--",
-  z: "--..",
-  " ": " "
+  a: " ·- ",
+  b: " -··· ",
+  c: " -·-· ",
+  d: " -·· ",
+  e: " · ",
+  f: " ··-· ",
+  g: " --· ",
+  h: " ···· ",
+  i: " ·· ",
+  j: " ·--- ",
+  k: " -·- ",
+  l: " ·-·· ",
+  m: " -- ",
+  n: " -· ",
+  o: " --- ",
+  p: " ·--· ",
+  q: "--.- ",
+  r: " ·-· ",
+  s: " ··· ",
+  t: " - ",
+  u: " ··- ",
+  v: " ···- ",
+  w: " ·-- ",
+  x: " -··- ",
+  y: " -·-- ",
+  z: " --·· ",
+  " ": " //  "
 }
 
 //grab html elements
@@ -34,7 +34,7 @@ const translateBtn = document.querySelector("button");
 const textInput = document.getElementById("textInput");
 const textOutput = document.querySelector(".morse__output__translation");
 
-//reference array
+//reference array for the morse code
 let letterKeys = Object.keys(morseCode);
 
 
@@ -43,40 +43,60 @@ const words = [];
 
 
 
-//how to translate from typed input to morse code
-
-
-
 //run translation  on button click
 const handleTranslate = translateBtn.addEventListener("click", (e) => {
   let input = textInput.value.toLowerCase();
+
   
   //validate input is valid - if yes push letter to array, if not message to try again
 const validInput = () => {
-  for(let i = 0; i < input.length; i++) {
-    if(letterKeys.includes(input[i])){
-      words.push(input[i]);
-    }else {
-       textInput.value= "";
-       document.querySelector(".morse__output__title").innerHTML = "Try Again!"
-    }
-  }
-}
+   const regex = /[^\w\s]/g
+   if(input.search(regex) != -1) {
+    document.querySelector(".morse__output__title").innerHTML = "Try Again!"
+   } else {
+      for(let i = 0; i < input.length; i++) {
+        let letter = input[i];
+        words.push(letter);
+      }  
+   }
+   }
+    
+  // for(let i = 0; i < input.length; i++) {
+  //   let letter = input[i];
+  //   if((letterKeys.includes(letter))) {
+  //     words.push(letter);
+  //     console.log(words)
+  //   } else {
+  //     document.querySelector(".morse__output__title").innerHTML = "Try Again!"
+  //   }
+  // }
+  // }
+
 
 const translatePhrase = () => {
   let translationArr = []
   words.forEach((word) => {
-    translationArr.push(morseCode[word]);
-    })
+  translationArr.push(morseCode[word]);
+  textOutput.innerHTML = translationArr.join(" ");
   
-  textOutput.innerHTML = translationArr.join("  ");
+    }
+  
+  )}
+  
+const clear = () => {
+  textInput.value= null;
+  words.length = 0;
+
 }
 
- validInput(); 
- translatePhrase();
 
 
+  
+  validInput(); 
+  translatePhrase();
+  clear();
 
+  
 })
 
 
